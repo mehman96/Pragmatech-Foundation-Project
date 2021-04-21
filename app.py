1571ac6e242ceac565e5c66454500805ad116c6a
mehmanmirzeyev <mehmanmirzeyev000@gmail.com>
2021-04-21 23:44:14 +0400

    update github

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
-a=5
-print(a)
+# # alqoritm 1, task cumlenin icinde nece herf var?
+# cumle="javascript ne vaxtdan dil olub, gedin duz emelli dil oyren!"
+
+# uzunluq= len(cumle)
+# bosluq= cumle.count(" ")
+# herfler= uzunluq-bosluq
+# print(herfler)
+
+# sozler= cumle.split(" ")
+
+# for x in sozler:
+#   print(x)
+
+#   len -leght sozunun qisasidi cumlenin icindrkilerin sayi ucun,
+# count -saymaq ucun 
+
+
+
+
