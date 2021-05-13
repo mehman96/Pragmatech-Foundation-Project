e2d59cbe1290c922a35c49577fadc131676a1e3e
mehmanmirzeyev <mehmanmirzeyev000@gmail.com>
2021-05-13 21:04:35 +0400

    pyton test

--- admin/routes.py
+++ admin/routes.py
@@ -5,32 +5,26 @@ import os
 
 from flask import render_template,redirect,request
 
-@app.route('/admin')
-def admin_index():
-    return render_template('/admin/index.html')
+# @app.route('/admin')
+#  def admin_index():     
+#     return render_template('/admin/index.html')
 
-@app.route('/admin/user',methods=['GET', 'POST']) 
-def admin_user_info():
-    test=Post.query.all()
+@app.route('/admin',methods=['GET', 'POST']) 
+def admin_user_():
+    posts=Contact.query.all()
     if request.method=='POST':
-        filename=file.filename
-        file==request.files['Adınız']
-        file==request.files['Email']
-        file==request.files['Əlaqə nömrəsi']
-        file==request.files['Mesaj buraxın']
-        file==request.files['Ünvan']
-        file==request.files['resume_link']
-        file==request.files['google_map']
-    return render_template('admin/user.html',test=test)
-    
-    test=test(
-          Adınız=request.form['Adınız'],
-          Email=request.form['Email'],
-          Əlaqənömrəsi=request.form['Əlaqə nömrəsi']
-          
-      )
+        post=Contact(
+          user_name=request.form['user_name'],
+          user_adress=request.form['user_adress'],
+          user_email=request.form['user_email'],
+          user_phone=request.form['user_phone'],
+          user_message=request.form['user_message'],
+          resume_link=request.form['resume_link'],
+          google_map=request.form['google_map']
+        )
 
-    db.session.add(test)
-    db.session.commit()
-    return redirect('/admin/user')
-    return render_template('admin/user.html', test=test)
+     db.session.add(posts)
+     db.session.commit()
+
+      return redirect('/admin')
+    return render_template('admin/user.html', posts=posts)
